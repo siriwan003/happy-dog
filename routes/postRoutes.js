ff428eb5033f453cb22914f39e75c06718966387
siriwan <siriwan.cha@chonvc.ac.th>
2025-09-18 16:45:21 +0700

    fix some bugs

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,23 +1,13 @@
 import { Router } from "express"
-import multer from "multer"
 import { createPost, deletePost, updatePost } from "../controllers/postController.js"
 
 const router = Router()
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/") 
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname)
-  },
-})
-const upload = multer({ storage })
 
 // router.get('/posts')
 // router.get('/posts/:id')
 router.put("/post/:id", updatePost)
-router.post("/posts", upload.single("image"), createPost)  
+router.post("/posts", createPost)  
 router.delete("/posts/:id", deletePost)
 
 export default router
